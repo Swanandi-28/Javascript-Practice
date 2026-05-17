f7dfbf9570d7b01626e9f5e0c4d76597c8d23815
Swanandi-28 <swanandi.0865@gmail.com>
2026-05-17 19:35:14 +0530

    To Do List

--- prac2/script.js
+++ prac2/script.js
@@ -1,14 +1,37 @@
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
 
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
+function addTask() {
+  if (taskInput.value === "") {
+    alert("Enter a task!");
+    return;
+  }
 
-    function AddTask() {
-    if (taskInput.value === '') {
-        alert("Please enter a task."); }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = taskInput.value;
-       "taskList".appendChild(li);
-    }
+  createTask(taskInput.value, false);
+  saveTasks();
+  taskInput.value = "";
 }
 
+function createTask(text, completed) {
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.innerText = text;
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "X";
+  deleteBtn.className = "delete-btn";
+
+  deleteBtn.onclick = function () {
+    li.remove();
+    saveTasks();
+  };
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+}
+
+
+
+  
